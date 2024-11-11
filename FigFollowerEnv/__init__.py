312766f85d05933b7941ca057d8facbc470e48cb
Michel Pescina <gatocorreo27@gmail.com>
2024-11-10 19:00:12 -0600

    Added an improved version of environment, the v1

--- FigFollowerEnv/__init__.py
+++ FigFollowerEnv/__init__.py
@@ -1,8 +1,14 @@
 from gymnasium.envs.registration import register
 from FigFollowerEnv.env.FigFollowerV0 import FigFollowerV0
+from FigFollowerEnv.env.FigFollowerV1 import FigFollowerV1
 from FigFollowerEnv.get_assets_path import get_assets_path
 
 register(
     id="FigFollowerEnv/FigFollower-v0",
     entry_point="FigFollowerEnv.env.FigFollowerV0:FigFollowerV0",
+)
+
+register(
+    id="FigFollowerEnv/FigFollower-v1",
+    entry_point="FigFollowerEnv.env.FigFollowerV1:FigFollowerV1",
 )
